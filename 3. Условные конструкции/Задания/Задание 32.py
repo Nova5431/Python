eda74b708370b16ccd5d17a04a0d0fdc449c9bce
Nova5431 <Jane-L19@yandex.ru>
2026-04-12 11:18:58 +0300

    решение задач

--- 3. Условные конструкции/Задания/Задание 32.py
+++ 3. Условные конструкции/Задания/Задание 32.py
@@ -2,7 +2,7 @@ a = int(input())
 b = int(input())
 c = int(input())
 d = int(input())
-x = int(1, -1, 0)
+
 if (a - c == 1 or a - c == -1 or a - c == 0) and (b - d == 1 or b - d == -1 or b - d == 0):
     print('YES')
 else:
