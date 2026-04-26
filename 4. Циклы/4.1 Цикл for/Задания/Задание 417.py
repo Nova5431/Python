61e59cb7d2eadfc7e804c5b9ea0c7c77beeab23d
Nova5431 <Jane-L19@yandex.ru>
2026-04-26 11:58:50 +0300

    решение задач

--- 4. Циклы/4.1 Цикл for/Задания/Задание 417.py
+++ 4. Циклы/4.1 Цикл for/Задания/Задание 417.py
@@ -1,1 +1,24 @@
+n = int(input())
+
+zero = 0
+negative = 0
+positive = 0
+x = 0
+
+
+
+
+for i in range(n):
+    m = int(input())
+
+    if m == 0:
+        zero += 1
+    elif m < 0:
+        negative += 1
+    elif m > 0:
+        positive += 1
+        x += m
+l = x/positive
+
+print(f'Нулей: {zero}\nOтрицательных: {negative}\nСреднее положительных: {l:.2f}')
 
